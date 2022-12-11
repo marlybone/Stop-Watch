c8e212b164d7de6e1481acc523aa981c7328bd7f
Phonique <41431476+marlybone@users.noreply.github.com>
2022-12-11 22:07:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var timer = 0;
+
+var timerInterval;
+
+var ms = document.getElementById('ms');
+var second = document.getElementById('second');
+var minute = document.getElementById('minute');
+
+function start() {
+  
+}
+
+
+
+function stop() {
+  
+}
